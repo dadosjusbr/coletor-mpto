881e09ca247c302f48f7f428b7edbf24febd2d28
marcos-vas <marcosvaas@gmail.com>
2021-12-20 10:22:11 -0300

    tests: testes para 8/2021

--- src/test_parser.py
+++ src/test_parser.py
@@ -87,6 +87,22 @@ class TestParser(unittest.TestCase):
         result_to_dict = MessageToDict(result_data)
         
         self.assertEqual(expected, result_to_dict)
+    
+    def test_aug_2021(self):
+        self.maxDiff = None
+        # Json com a saida esperada
+        with open('src/output_test/expected/expected_08_2021.json', 'r') as fp:
+            expected = json.load(fp)
+
+        files = ['src/output_test/sheets/membros-ativos-contracheque-08-2021.ods',
+                 'src/output_test/sheets/membros-verbas-indenizatorias-08-2021.ods']
+
+        dados = load(files, '2021', '08', 'src/output_test/sheets')
+        result_data = parse(dados, 'mpto/8/2021', '08', '2021')
+        # Converto o resultado do parser, em dict
+        result_to_dict = MessageToDict(result_data)
+    
+        self.assertEqual(expected, result_to_dict)
 
 
 if __name__ == '__main__':
